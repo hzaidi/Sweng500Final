de88e8b92b484a6f7e7a6cefd62f7b0488f77f2f
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2016-09-19 17:58:15 -0400

    fix of showing the org name in the organization detail page.

--- app/components/organization/cmpt-organization-details/organizationDetails.js
+++ app/components/organization/cmpt-organization-details/organizationDetails.js
@@ -9,15 +9,18 @@ templateUrl: '/components/organization/cmpt-organization-details/organizationDet
 
 // #-----------------------------------------------# //
 // #---- Component (cmpt-organization-details) ----# //
-controller: function ($scope, userSvc) {
+controller: function ($scope, organizationSvc) {
 
 
-	var user = userSvc.userObj($scope.user);
+
 	// View Model properties
 	var vm = $scope.vm = {
-		org: user.org
+		org: null
 	};
 
+	organizationSvc.getOrg().then(function(org){
+		vm.org = organizationSvc.orgObj(org);
+	});
 	// Actions that can be bound to from the view
 	var go = $scope.go = {
 		updateOrg: function () {
