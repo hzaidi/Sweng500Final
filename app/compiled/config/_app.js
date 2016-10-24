601af375ba96ab3123980c9e5052b1c83bde48ba
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2016-10-24 08:09:49 -0400

    Testing.

--- app/compiled/config/_app.js
+++ app/compiled/config/_app.js
@@ -3,9 +3,7 @@
 (function () {
 	'use strict';
 	var app = angular.module('piStatus', ['ui.router', 'firebase', 'ngResource', 'ngSanitize', 'ngAnimate', 'templates-preload', 'ngDialog', 'toastr', 'ngMessages', 'chart.js', '720kb.datepicker']);
-
 	app.config(function (ChartJsProvider) {
-		firebase.initializeApp(window.siteConfig);
 		ChartJsProvider.setOptions('doughnut', {
 			cutoutPercentage: 80
 		});
