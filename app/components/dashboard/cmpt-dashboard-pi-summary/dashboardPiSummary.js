966a386f295c94e6bf21ae6e59643a946c79bab2
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2016-11-14 18:01:14 -0500

    sprint should not be negative.

--- app/components/dashboard/cmpt-dashboard-pi-summary/dashboardPiSummary.js
+++ app/components/dashboard/cmpt-dashboard-pi-summary/dashboardPiSummary.js
@@ -38,7 +38,7 @@ controller: function ($scope, $filter, $interval, dashboardSvc, programIncrement
 	function timeLeft(endDate, lenOfSprint) {
 		var numDaysLeft = dateHelp.daysLeft(new Date(endDate), vm.tomorrow);
 		var sprintsLeft = Math.ceil(dateHelp.weeksLeft(new Date(endDate), new Date())/lenOfSprint);
-		vm.sprintsLeft = sprintsLeft;
+		vm.sprintsLeft = sprintsLeft <= 0 ? 0 : sprintsLeft;
 		vm.daysLeft = numDaysLeft <= 0 ? 0 : numDaysLeft;
 		if(numDaysLeft <= 0) { $interval.cancel(calculateSummaryTimer); }
 	}
